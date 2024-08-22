d7b018463667aedb00dcd20ebb8b52ce1dbbf7b4
AdmiN <AdmiN@ITWS0039>
2024-08-22 10:25:30 +0530

    commit

---
+++ src/demo_pack/Demo538.java
@@ -0,0 +1,8 @@
+package demo_pack;
+
+public class Demo538 {
+
+	public static void main(String args[]) {
+		System.out.println("Hello universe");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Devops_demo {
+}
